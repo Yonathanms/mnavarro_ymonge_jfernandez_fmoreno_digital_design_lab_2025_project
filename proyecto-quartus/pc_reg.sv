6ffdb56585018f296ea6aa9d0c2d5c22657c8964
Jopa1474 <jo.fernandez@estudiantec.cr>
2025-11-06 18:10:01 -0600

    PC

---
+++ proyecto-quartus/pc_reg.sv
@@ -0,0 +1,15 @@
+module pc_reg (
+    input  logic        clk,
+    input  logic        rst,
+    input  logic [31:0] pc_next,
+    output logic [31:0] pc
+);
+
+    always_ff @(posedge clk or posedge rst) begin
+        if (rst)
+            pc <= 32'h0000_0000;
+        else
+            pc <= pc_next;
+    end
+
+endmodule
