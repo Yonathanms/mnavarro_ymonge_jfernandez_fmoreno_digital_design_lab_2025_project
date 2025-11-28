2be59c30c7c8bc6fcbe4f366fc1833310c9c3372
yonathan_ms146 <yonathan0146@estudiantec.cr>
2025-11-28 05:44:53 -0600

    Se creó RAM, ROM, VRAM
    Se agregaron memorias con características pensadas para no tener problemas en el futuro.
    Justificación: Un mal manejo o creación de las memorias inducen a errores irreparables en una etapa muy avanzada del proyecto

---
+++ proyecto-quartus/vram_2puertos_module_inst.v
@@ -0,0 +1,8 @@
+vram_2puertos_module	vram_2puertos_module_inst (
+	.clock ( clock_sig ),
+	.data ( data_sig ),
+	.rdaddress ( rdaddress_sig ),
+	.wraddress ( wraddress_sig ),
+	.wren ( wren_sig ),
+	.q ( q_sig )
+	);
